ce6baf91e3e063fac62d3c6246226f47e59c3cef
hemantjadon <hemantjadon.hemu@gmail.com>
2020-06-26 00:02:53 +0530

    refactor(main): use new proxy and cli layer at top level

--- main.go
+++ main.go
@@ -3,16 +3,23 @@ package main
 
 import (
 	"context"
-	"flag"
+	"errors"
 	"fmt"
 	"io"
+	"io/ioutil"
+	"log"
+	"net"
 	"net/http"
 	"os"
 	"os/signal"
 	"path/filepath"
-	"strings"
 	"time"
+
+	"github.com/hemantjadon/gosrv/internal/pkg/cli"
+	"github.com/hemantjadon/gosrv/internal/pkg/proxy"
 )
+
+const AppName = "gosrv"
 
 func main() {
 	ctx := context.Background()
@@ -27,111 +34,173 @@ func main() {
 		select {
 		case <-sigChan:
 			cancel()
-			_, _ = fmt.Fprintf(os.Stdout, "closing down, press ctrl+c again to force quit\n")
+			_, _ = fmt.Fprintf(os.Stdout, "%s: closing down, press ctrl+c again to force quit\n", AppName)
 		case <-ctx.Done():
 		}
 		<-sigChan
 		os.Exit(exitCodeOnInterrupt)
 	}()
 	if err := run(ctx, os.Args[1:], os.Stdout, os.Stderr); err != nil {
-		_, _ = fmt.Fprintf(os.Stderr, "%s\n", err.Error())
+		if len(err.Error()) != 0 {
+			_, _ = fmt.Fprintf(os.Stderr, "%s: %s\n", AppName, err.Error())
+		}
 		os.Exit(exitCodeOnErr)
 	}
 }
 
 func run(ctx context.Context, args []string, stdout, stderr io.Writer) error {
-	fs := flag.NewFlagSet("gosrv", flag.ContinueOnError)
-	help := fs.Bool("help", false, "show help")
-	host := fs.String("host", "localhost", "host name")
-	port := fs.String("port", "8080", "port number")
-	cert := fs.String("cert", "", "SSL cert")
-	key := fs.String("key", "", "SSL cert key")
-	dir := fs.String("dir", ".", "directory to serve")
-
-	if err := fs.Parse(args); err != nil {
-		return fmt.Errorf("unable to parse options: %w", err)
+	inputs, err := cli.Parse(AppName, args)
+	if err != nil {
+		_, _ = fmt.Fprintf(stderr, "%s: cli: %s\n", AppName, err.Error())
+		cli.WriteHelp(stderr, AppName)
+		return fmt.Errorf("")
 	}
 
-	if help != nil && *help {
-		fs.SetOutput(os.Stdout)
-		_, _ = fmt.Fprintf(os.Stdout, "Usage of gosrv:\n")
-		fs.PrintDefaults()
+	if inputs.Help {
+		cli.WriteHelp(stderr, AppName)
 		return nil
 	}
 
-	if host == nil || port == nil || dir == nil || *host == "" || *port == "" || *dir == "" {
-		return fmt.Errorf("host, port and dir are required")
-	}
-
-	if (key == nil || *key == "") && (cert != nil && *cert != "") {
-		return fmt.Errorf("key is required along with the cert")
-	}
-	if (key != nil && *key != "") && (cert == nil || *cert == "") {
-		return fmt.Errorf("cert is required along with the key")
-	}
-
-	dirAbsPath, err := filepath.Abs(*dir)
+	dirAbsPath, err := filepath.Abs(inputs.Dir)
 	if err != nil {
 		return fmt.Errorf("unable to determine dir absolute path: %w", err)
 	}
 
-	cleartext := (key == nil || *key == "") && (cert == nil || *cert == "")
+	cleartext := (len(inputs.KeyFile) == 0) && (len(inputs.CertFile) == 0)
 
-	addr := strings.Join([]string{*host, *port}, ":")
-	srv := setupHTTP(addr, dirAbsPath)
+	httpLis, err := setupHTTPListener(inputs.Host)
+	if err != nil {
+		return fmt.Errorf("setup http tcp listener: %w", err)
+	}
+	defer httpLis.Close()
 
+	httpHost, httpPort, err := net.SplitHostPort(httpLis.Addr().String())
+	if err != nil {
+		return fmt.Errorf("http tcp listener addr split host port: %w", err)
+	}
+	httpAddr := net.JoinHostPort(httpHost, httpPort)
+
+	var logger *log.Logger
+	if inputs.Quiet {
+		logger = setupLogger(ioutil.Discard)
+	} else {
+		logger = setupLogger(stderr)
+	}
+
+	proxyAddr := net.JoinHostPort(inputs.Host, inputs.Port)
+	prx := setupProxy(proxyAddr, httpAddr, !cleartext, logger)
+
+	srv := setupHTTPServer(dirAbsPath)
+
+	srvClosedCh := make(chan struct{})
 	go func() {
+		defer close(srvClosedCh)
 		handle := func(err error) {
 			if err != nil {
-				_, _ = fmt.Fprintf(stderr, "start server: %s\n", err.Error())
+				_, _ = fmt.Fprintf(stderr, "%s: error: start server: %s\n", AppName, err.Error())
 				return
 			}
-			_, _ = fmt.Fprintf(stdout, "closed server\n")
+			_, _ = fmt.Fprintf(stdout, "%s: closed server\n", AppName)
 		}
 
 		if cleartext {
-			_, _ = fmt.Fprintf(stdout, "starting http server on %s\n", srv.Addr)
-			handle(runHTTP(srv))
+			_, _ = fmt.Fprintf(stdout, "%s: starting http server on %s\n", AppName, proxyAddr)
+			handle(runHTTP(httpLis, srv))
 		} else {
-			_, _ = fmt.Fprintf(stdout, "starting https server on %s\n", srv.Addr)
-			handle(runHTTPTLS(srv, *cert, *key))
+			_, _ = fmt.Fprintf(stdout, "%s: starting https server on %s\n", AppName, proxyAddr)
+			handle(runHTTPTLS(httpLis, srv, inputs.CertFile, inputs.KeyFile))
 		}
 	}()
 
-	<-ctx.Done()
-	if err := stopHTTP(srv); err != nil {
-		_, _ = fmt.Fprintf(stderr, "stop server: %s\n", err.Error())
+	prxClosedCh := make(chan struct{})
+	go func() {
+		defer close(prxClosedCh)
+
+		if err := prx.ListenAndTransfer(); err != nil && !errors.Is(err, proxy.ErrClosed) {
+			_, _ = fmt.Fprintf(stderr, "%s: error: running proxy: %s\n", AppName, err.Error())
+		}
+	}()
+
+	closedCh := make(chan struct{})
+	closingCh := make(chan struct{})
+	go func() {
+		defer close(closedCh)
+		select {
+		case <-prxClosedCh:
+			if err := srv.Shutdown(context.Background()); err != nil {
+				_, _ = fmt.Fprintf(stderr, "%s: error: stop server: %s\n", AppName, err.Error())
+			}
+		case <-srvClosedCh:
+			if err := prx.Shutdown(context.Background()); err != nil {
+				_, _ = fmt.Fprintf(stderr, "%s: error: closing proxy: %s\n", AppName, err.Error())
+			}
+		case <-closingCh:
+		}
+	}()
+
+	select {
+	case <-closedCh:
+		return nil
+	case <-ctx.Done():
 	}
+
+	close(closingCh)
+
+	if err := prx.Shutdown(context.Background()); err != nil {
+		_, _ = fmt.Fprintf(stderr, "%s: error: closing proxy: %s\n", AppName, err.Error())
+	}
+
+	if err := srv.Shutdown(context.Background()); err != nil {
+		_, _ = fmt.Fprintf(stderr, "%s: error: stop server: %s\n", AppName, err.Error())
+	}
+
 	return nil
 }
 
-func setupHTTP(addr, dir string) *http.Server {
-	return &http.Server{
-		Addr:         addr,
-		Handler:      http.FileServer(http.Dir(dir)),
-		ReadTimeout:  5 * time.Second,
-		WriteTimeout: 1 * time.Minute,
-		IdleTimeout:  5 * time.Second,
-	}
+func setupLogger(w io.Writer) *log.Logger {
+	return log.New(w, "", log.Ldate|log.Ltime|log.Lmicroseconds)
 }
 
-func runHTTP(srv *http.Server) error {
-	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+func setupProxy(proxyAddr, upstreamAddr string, upstreamTLS bool, logger *log.Logger) *proxy.Proxy {
+	prx := proxy.Proxy{
+		Network:         "tcp",
+		Addr:            proxyAddr,
+		UpstreamNetwork: "tcp",
+		UpstreamAddr:    upstreamAddr,
+		UpstreamTLS:     upstreamTLS,
+		Logger:          logger,
+	}
+	return &prx
+}
+
+func setupHTTPListener(host string) (net.Listener, error) {
+	l, err := net.Listen("tcp", net.JoinHostPort(host, "0"))
+	if err != nil {
+		return nil, fmt.Errorf("net listen tcp: %w", err)
+	}
+	return l, nil
+}
+
+func setupHTTPServer(dir string) *http.Server {
+	srv := http.Server{
+		Handler:      http.FileServer(http.Dir(dir)),
+		ReadTimeout:  readTimeout,
+		WriteTimeout: writeTimeout,
+		IdleTimeout:  idleTimeout,
+	}
+	return &srv
+}
+
+func runHTTP(l net.Listener, srv *http.Server) error {
+	if err := srv.Serve(l); err != nil && err != http.ErrServerClosed {
 		return fmt.Errorf("listen and serve: %w", err)
 	}
 	return nil
 }
 
-func runHTTPTLS(srv *http.Server, certFile, keyFile string) error {
-	if err := srv.ListenAndServeTLS(certFile, keyFile); err != nil && err != http.ErrServerClosed {
+func runHTTPTLS(l net.Listener, srv *http.Server, certFile, keyFile string) error {
+	if err := srv.ServeTLS(l, certFile, keyFile); err != nil && err != http.ErrServerClosed {
 		return fmt.Errorf("listen and serve tls: %w", err)
-	}
-	return nil
-}
-
-func stopHTTP(srv *http.Server) error {
-	if err := srv.Shutdown(context.Background()); err != nil {
-		return fmt.Errorf("shutdown: %w", err)
 	}
 	return nil
 }
@@ -139,4 +208,10 @@ func stopHTTP(srv *http.Server) error {
 const (
 	exitCodeOnErr       = 1
 	exitCodeOnInterrupt = 2
+)
+
+const (
+	readTimeout  = 5 * time.Second
+	idleTimeout  = 5 * time.Second
+	writeTimeout = 60 * time.Second
 )
